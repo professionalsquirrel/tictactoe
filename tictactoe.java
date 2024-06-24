c0a72a7c093fbfd58bb004c16b3a8ef85e238c2b
professionalsquirrel <71053041+professionalsquirrel@users.noreply.github.com>
2024-06-24 12:37:02 +0100

    Add piece to gameboard

--- tictactoe.java
+++ tictactoe.java
@@ -1,6 +1,8 @@
 /**
  * tictactoe
  */
+import java.util.Scanner;
+
 public class tictactoe {
 
     public static void main(String[] args){
@@ -11,7 +13,13 @@ public class tictactoe {
         {' ', '|', ' ', '|', ' '}
     };
     printGameBoard(gameBoard);
-    
+
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter your placement (1-9):");
+    int pos = scan.nextInt();
+    System.out.println(pos);
+    placePiece(gameBoard, pos, "player");
+    printGameBoard(gameBoard);
     }
 
     public static void printGameBoard(char[][] gameBoard){
@@ -20,6 +28,49 @@ public class tictactoe {
                 System.out.print(c);
             }
             System.out.println();
+        }
+    }
+
+    public static void placePiece(char[][] gameBoard, int pos, String user){
+        
+        char symbol = 'X';
+
+        if(user.equals("player")){
+            symbol = 'X';
+        } else if(user.equals("cpu")){
+            symbol = '0';
+        }
+
+        switch(pos){
+            case 1:
+                gameBoard[0][0]=symbol;
+                break;
+            case 2:
+                gameBoard[0][2]=symbol;
+                break;
+            case 3:
+                gameBoard[0][4]=symbol;
+                break;
+            case 4:
+                gameBoard[2][0]=symbol;
+                break; 
+            case 5:
+                gameBoard[2][2]=symbol;
+                break;
+            case 6:
+                gameBoard[2][4]=symbol;
+                break;
+            case 7:
+                gameBoard[4][0]=symbol;
+                break; 
+            case 8:
+                gameBoard[4][2]=symbol;
+                break;
+            case 9:
+                gameBoard[4][4]=symbol;
+                break;   
+            default:
+                break; 
         }
     }
 }
